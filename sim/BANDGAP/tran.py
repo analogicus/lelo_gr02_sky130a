a53bdcda8f68620129714a51295c9df12a89e379
Simen Eilevstjønn <simene@lyse.net>
2026-02-06 13:36:59 +0100

    Add plot of vctat and iptat in tran.py

--- sim/BANDGAP/tran.py
+++ sim/BANDGAP/tran.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import pandas as pd
 import yaml
+import matplotlib.pyplot as plt
 
 def main(name):
   # Delete next line if you want to use python post processing
@@ -12,7 +13,30 @@ def main(name):
     obj = yaml.safe_load(fi)
 
   # Do something to parameters
+  iptat = []
+  vctat = []
+
+  for key, val in obj.items():
+    if "iptat" in key:
+      iptat.append((int(key[6:]), val))
+    elif "vctat" in key:
+      vctat.append((int(key[6:]), val))
+
+  iptat.sort(key=lambda x : x[0])
+  vctat.sort(key=lambda x : x[1])
+
+  plt.plot([temp for temp, val in iptat], [val for temp, val in iptat])
+  plt.title("I PTAT")
+  plt.xlabel("Temperature [C]")
+  plt.ylabel("Current [A]")
+  plt.figure()
+  plt.plot([temp for temp, val in vctat], [val for temp, val in vctat])
+  plt.title("V CTAT")
+  plt.xlabel("Temperature [C]")
+  plt.ylabel("Voltage [V]")
 
   # Save new yaml file
-  with open(yamlfile,"w") as fo:
-    yaml.dump(obj,fo)
+  # with open(yamlfile,"w") as fo:
+  #   yaml.dump(obj,fo)
+
+  plt.show()
