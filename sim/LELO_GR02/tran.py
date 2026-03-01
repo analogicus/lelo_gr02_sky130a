ab6a4cebe6eaea75e07af3842ed3f27a8f4187b9
Simen Eilevstjønn <simene@lyse.net>
2026-03-01 09:42:08 +0100

    Add leakage current measurement and current usage updated. Fix pwrup

--- sim/LELO_GR02/tran.py
+++ sim/LELO_GR02/tran.py
@@ -13,37 +13,24 @@ def main(name):
     obj = yaml.safe_load(fi)
 
   # Do something to parameters
-  i_vdd = []
-  vdd = []
   t1 = []
   t2 = []
 
   for key, val in obj.items():
-    if "i_vdd" in key:
-      i_vdd.append((int(key[6:]), val))
-    elif "vdd" in key:
-      vdd.append((int(key[4:]), val))
-    elif "t1" in key:
+    if "t1" in key:
       t1.append((int(key[3:]), val))
     elif "t2" in key:
       t2.append((int(key[3:]), val))
 
-  i_vdd.sort(key = lambda x : x[0])
-  vdd.sort(key = lambda x : x[0])
   t1.sort(key = lambda x : x[0])
   t2.sort(key = lambda x : x[0])
 
-  temps = [t for t, _ in i_vdd]
+  temps = [t for t, _ in t1]
 
-  i_vdd = [v for _, v in i_vdd]
-  vdd = [v for _, v in vdd]
   freqs = [10 / (b - a) for (_,a), (_,b) in zip(t1, t2)]
 
   for t, f in zip(temps, freqs):
     obj[f"freq_{t}"] = f
-
-  for t, v, i  in zip(temps, vdd, i_vdd):
-    obj[f"pwr_{t}"] = -v * i
 
   if DO_PLOT:
     plt.plot(temps, freqs)
