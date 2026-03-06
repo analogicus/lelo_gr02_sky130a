1a54257d27d3aa6076394110f41cf3a7d5b75430
Simen Eilevstjønn <simene@lyse.net>
2026-03-06 11:55:08 +0100

    Add sparse simulation and alter tran.py and replace.yaml params

--- sim/LELO_GR02/tran.py
+++ sim/LELO_GR02/tran.py
@@ -3,7 +3,7 @@ import yaml
 import matplotlib.pyplot as plt
 import scipy as sp
 
-DO_PLOT = True
+DO_PLOT = False
 
 def main(name):
   yamlfile = name + ".yaml"
@@ -44,6 +44,13 @@ def main(name):
   obj["freq_intercept"] = float(lm.intercept)
   obj["freq_slope"] = float(lm.slope)
   obj["freq_rvalue"] = float(lm.rvalue)
+
+  max_sq_err = max((freq - (lm.intercept - lm.slope * temp)**2) for temp, freq in zip(temps, freqs))
+
+  obj["freq_max_sq_err"] = float(max_sq_err)
+  fs = max(temps) - min(temps)
+  obj["freq_fs"] = float(fs)
+  obj["freq_max_sq_err_per_fs"] = float(max_sq_err / fs)
 
 
   # Save new yaml file
